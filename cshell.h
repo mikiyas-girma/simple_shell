b087068fd0eea7f6fccff29a93403c7cdf0bb9a5
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-12-13 16:40:06 +0300

    created header file to write headers and prototypes in it

---
+++ cshell.h
@@ -0,0 +1,17 @@
+#ifndef __CSHELL_H
+#define __CSHELL_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+
+
+
+
+
+#endif /*__CSHELL_H */
