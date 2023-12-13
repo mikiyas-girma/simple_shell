e9f9ca025c739f3e6a725f1b5a5748b227440b3a
mikiyas girma <mikiyasgirmaet@gmail.com>
2023-12-14 00:34:17 +0300

    betty styled

--- get_path.c
+++ get_path.c
@@ -1,5 +1,4 @@
 #include "cshell.h"
-
 /**
  * find_path -  function that points to a location of a path
  * @loc: location of the path
@@ -12,37 +11,38 @@
 
 char *find_path(char *loc, char *argv, char *av, int mod, int ctr)
 {
-    char *tmp = my_strdup(loc);
-    if (tmp == NULL)
-        return NULL;
+	char *tmp = my_strdup(loc);
 
-    char *delim = ":";
-    char *tok = my_strtok(tmp, delim);
+	if (tmp == NULL)
+		return (NULL);
 
-    while (tok != NULL)
-    {
-        char *move = (char *)malloc(my_strlen(tok) + my_strlen(argv) + 3);
-        if (move == NULL)
-        {
-            free(tmp);
-            return NULL;
-        }
+	char *delim = ":";
+	char *tok = my_strtok(tmp, delim);
 
-        my_strcpy(move, tok);
-        my_strcat(move, "/");
-        my_strcat(move, argv);
+	while (tok != NULL)
+	{
+		char *move = (char *)malloc(my_strlen(tok) + my_strlen(argv) + 3);
 
-        if (access(move, X_OK) == 0)
-        {
-            free(tmp);
-            return move;
-        }
+		if (move == NULL)
+		{
+			free(tmp);
+			return (NULL);
+		}
 
-        free(move);
-        tok = my_strtok(NULL, delim);
-    }
+		my_strcpy(move, tok);
+		my_strcat(move, "/");
+		my_strcat(move, argv);
 
-    free(tmp);
-    return NULL;
+		if (access(move, X_OK) == 0)
+		{
+			free(tmp);
+			return (move);
+		}
+
+		free(move);
+		tok = my_strtok(NULL, delim);
+	}
+
+	free(tmp);
+	return (NULL);
 }
-
