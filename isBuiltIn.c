1dccf101796d9ea94a34f0ed6ce9dc6efdc9e395
meliance <melakmu66@gmail.com>
2023-12-13 12:58:20 -0800

    errors are fixed

--- isBuiltIn.c
+++ isBuiltIn.c
@@ -9,24 +9,22 @@
  */
 int isBuiltInCmd(char *buff, char **argv)
 {
-    if (my_strcmp(argv[0], "exit") == 0)
-    {
-        free(buff);
-        fr_argv(argv);
-        exit(0);
-    }
-    else if (my_strcmp(argv[0], "cd") == 0)
-    {
-        chdir((argv[1] == NULL) ? findEnvVar("HOME") : argv[1]);
-        return 1;
-    }
-    else if (my_strcmp(argv[0], "envr") == 0)
-    {
-        envVar();
-        return 1;
-    }
+	if (my_strcmp(argv[0], "exit") == 0)
+	{
+		free(buff);
+		fr_argv(argv);
+		exit(0);
+	}
+	else if (my_strcmp(argv[0], "cd") == 0)
+	{
+		chdir((argv[1] == NULL) ? findEnvVar("HOME") : argv[1]);
+		return (1);
+	}
+	else if (my_strcmp(argv[0], "envr") == 0)
+	{
+		envVar();
+		return (1);
+	}
 
-    return 0;
+	return (0);
 }
-
-
